f986a8dd2a010299f90acb64da83f68449bd2515
taekitahata <ttaakeagi.06032115-u.u@wzweb.ne.jp>
2019-09-13 10:23:26 +0900

    applicationファイル訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_042259) do
+ActiveRecord::Schema.define(version: 2019_09_12_071906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
