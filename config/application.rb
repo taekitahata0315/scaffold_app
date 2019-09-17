f5b7fdaf7634af713c6c3bc4fc28671085ab2ec1
taekitahata <ttaakeagi.06032115-u.u@wzweb.ne.jp>
2019-09-17 14:02:58 +0900

    日時の日本時間への変更

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ScaffoldApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.generators.template_engine = :slim 
-
+    config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
