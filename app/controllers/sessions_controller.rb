9f20386625ffb42e8ce85e3490b8bcdfc88011f4
taekitahata <ttaakeagi.06032115-u.u@wzweb.ne.jp>
2019-09-19 12:56:37 +0900

    sing_up.fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password)
   end
 end
 
