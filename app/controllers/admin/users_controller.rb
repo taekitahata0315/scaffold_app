819fed41e08b8b16a703a2ae970ace46bdf9f824
taekitahata <ttaakeagi.06032115-u.u@wzweb.ne.jp>
2019-09-20 12:40:42 +0900

    ユーザー.fix

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,7 +22,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_users_path, notice:"ユーザー「#{@user.name}」を登録しました。"
+      redirect_to root_path, notice:"ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
