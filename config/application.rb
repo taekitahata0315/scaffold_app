0d5f1c59b00c222a8066e18e5c7d441e8eac1974
taekitahata <ttaakeagi.06032115-u.u@wzweb.ne.jp>
2019-09-21 10:14:35 +0900

    登録日時・更新日時の表示変更

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module ScaffoldApp
     # Initialize configuration defaults for originally generated Rails version.
     config.generators.template_engine = :slim 
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
